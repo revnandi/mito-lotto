1a2ac6c67b5177dca3b673c811749c87b4ae73d5
revnandi <revnandi@gmail.com>
2023-05-06 14:13:18 +0200

    fix number formatting

--- src/components/resultsBox.tsx
+++ src/components/resultsBox.tsx
@@ -7,16 +7,23 @@ interface Props {
 };
 
 const ResultsBox = ({ numberOfTickets, yearsSpent, costOfTickets }: Props ) => {
-  const formatter = new Intl.NumberFormat('hu-Hu', {
+  const currencyFormatter = new Intl.NumberFormat('hu-Hu', {
     style: 'currency',
     currency: 'HUF',
   });
 
+  const ticketFormatter = new Intl.NumberFormat('hu-Hu', {
+    style: 'decimal',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+    useGrouping: true,
+  });
+
   return (
     <div className={ styles.resultsBox }>
-      <div className={ [styles.label, styles.labelIsHeavy].join(' ') }>Number of tickets: <span className={ styles.value }>{ numberOfTickets }</span></div>
+      <div className={ [styles.label, styles.labelIsHeavy].join(' ') }>Number of tickets: <span className={ styles.value }>{ ticketFormatter.format(numberOfTickets) }</span></div>
       <div className={ styles.label }>Years spent: <span className={ styles.value }>{ yearsSpent }</span></div>
-      <div className={ styles.label }>Cost of tickets: <span className={ styles.value }>{ formatter.format(costOfTickets) }</span></div>
+      <div className={ styles.label }>Cost of tickets: <span className={ styles.value }>{ currencyFormatter.format(costOfTickets) }</span></div>
     </div>
   );
 };
